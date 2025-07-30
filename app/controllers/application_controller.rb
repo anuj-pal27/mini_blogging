be7b7e04a6ee92c3b849f1ce209cb93880d2d6c1
anuj-pal27 <anujpal27669@gmail.com>
2025-07-30 14:01:41 +0530

    devise and authentcaiton setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
   allow_browser versions: :modern
 end
